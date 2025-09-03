ac7e4445613cc16a9c120a1edd82e35075eb3893
Matheus Calmon <sanderhus15@outlook.com>
2025-09-03 13:41:49 -0300

    exercicios estrutura de decisao

--- estrutura-decisao/ex08.py
+++ estrutura-decisao/ex08.py
@@ -6,9 +6,9 @@ produto1 = float(input("Digite o valor do primeiro produto: "))
 produto2 = float(input("Digite o valor do segundo produto: "))
 produto3 = float(input("Digite o valor do terceiro produto: "))
 
-if produto1 < produto2 and produto1 < produto2:
-    print(f"Voce deve comprar o primeiro produto.")
-elif produto2 > produto1 and produto2 > produto3:
-    print("Voce deve comprar o segundo produto")
+if produto1 < produto2 and produto1 < produto3:
+    print("Você deve comprar o primeiro produto.")
+elif produto2 < produto1 and produto2 < produto3:
+    print("Você deve comprar o segundo produto.")
 else:
-    print(f"Voce deve comprar o terceiro produto")
+    print("Você deve comprar o terceiro produto.")
