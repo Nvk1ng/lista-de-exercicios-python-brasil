d910078d050d1154e2b330f8401d269ee671e89b
matheus sanderhus <sanderhus15@outlook.com>
2025-10-14 12:19:48 -0300

    exercicios estrutura repeticao

---
+++ estrutura-repeticao/ex01.py
@@ -0,0 +1,26 @@
+"""Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo
+até que o usuário informe um valor válido.
+
+    >>> from secao_03_estrutura_de_repeticao import ex_01_numero_valido
+    >>> ex_01_numero_valido.input = lambda k: '0'
+    >>> ex_01_numero_valido.obter_numero_valido()
+    0
+    >>> entradas = ['1', 'a']
+    >>> ex_01_numero_valido.input = lambda k: entradas.pop()
+    >>> ex_01_numero_valido.obter_numero_valido()
+    Número inválido: a
+    1
+    >>> entradas = ['0', 'a', '-1']
+    >>> ex_01_numero_valido.input = lambda k: entradas.pop()
+    >>> ex_01_numero_valido.obter_numero_valido()
+    Número inválido: -1
+    Número inválido: a
+    0
+    >>> entradas = ['10', 'a', '-1', '11']
+    >>> ex_01_numero_valido.input = lambda k: entradas.pop()
+    >>> ex_01_numero_valido.obter_numero_valido()
+    Número inválido: 11
+    Número inválido: -1
+    Número inválido: a
+    10
+"""
---
+++ estrutura-repeticao/ex02.py
@@ -0,0 +1,14 @@
+"""Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma
+mensagem de erro e voltando a pedir as informações.
+
+    >>> from secao_03_estrutura_de_repeticao import ex_02_validador_de_senha
+    >>> credenciais = ['senha', 'meu_login']
+    >>> ex_02_validador_de_senha.input = lambda k: credenciais.pop()
+    >>> ex_02_validador_de_senha.validar_senha()
+    'Cadastro realizado com suceso, seu login é meu_login'
+    >>> credenciais = ['outra_senha', 'outro_login', 'igual', 'igual']
+    >>> ex_02_validador_de_senha.input = lambda k: credenciais.pop()
+    >>> ex_02_validador_de_senha.validar_senha()
+    Senha deve ser diferente do login
+    'Cadastro realizado com suceso, seu login é outro_login'
+"""
