f7edda4ac5de9f381fafabfc083757b33f019ec0
matheus sanderhus <sanderhus15@outlook.com>
2025-10-15 12:31:02 -0300

    exercicios estrutura repeticao

--- estrutura-repeticao/ex03.py
+++ estrutura-repeticao/ex03.py
@@ -31,3 +31,34 @@
     Erro: o sexo precisa ser "m" ou "f", não pode ser "t"
     Erro: o estado civil precisa ser "s", "c", "v" ou "d", não pode ser "p"
 """
+
+nome = input("Digite o nome: ")
+idade = int(input("Digite a idade: "))
+salario = float(input("Digite o salário: "))
+sexo = input("Digite o sexo (f/m): ")
+estado_civil = input("Digite o estado civil (s/c/v/d): ")
+
+erros = False
+
+if len(nome) <= 3:
+    print(f'Erro: o nome precisa ter 3 letras ou mais, não pode ser {nome}')
+    erros = True
+
+if idade < 0 or idade > 150:
+    print(f'Erro: a idade precisa estar entre 0 e 150, não pode ser {idade}')
+    erros = True
+
+if salario <= 0:
+    print(f'Erro: o salário precisa ser positivo, não pode ser {salario}')
+    erros = True
+
+if sexo not in ['f', 'm']:
+    print(f'Erro: o sexo precisa ser "m" ou "f", não pode ser "{sexo}"')
+    erros = True
+
+if estado_civil not in ['s', 'c', 'v', 'd']:
+    print(f'Erro: o estado civil precisa ser "s", "c", "v" ou "d", não pode ser "{estado_civil}"')
+    erros = True
+
+if not erros:
+    print("Cadastro realizado com sucesso")
